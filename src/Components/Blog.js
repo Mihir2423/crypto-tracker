092f43ec9e7a79b5522085cc5d495b6ca1581918
Mihir02324 <93427284+Mihir2423@users.noreply.github.com>
2023-02-21 19:27:48 +0530

    updated blog

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,16 +1,18 @@
-import React from "react"
+import React from "react";
 export default function Blog(props) {
-    return (
-        <div className="_blog">
-            <div className="blog_header">
-                <h1>{props.name}</h1>
-                <img src={props.main_image} alt="noimage" />
-            </div>
-            <p>{props.description}</p>
-            <div className="blog_footer">
-                <img src={props.provider_image} alt="noOne" />
-                <p>{props.date}</p>
-            </div>
+  return (
+    <a href={props.url} target="_blank" rel="noreferrer">
+      <div className="_blog">
+        <div className="blog_header">
+          <h1>{props.name}</h1>
+          <img src={props.main_image} alt="noimage" />
         </div>
-    )
+        <p>{props.description}</p>
+        <div className="blog_footer">
+          <img src={props.provider_image} alt="noOne" />
+          <p>{props.date}</p>
+        </div>
+      </div>
+    </a>
+  );
 }
